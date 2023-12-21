c3416d41312d51afcaccb5f0daca08d13d51e182
amitaccio <142372667+amitaccio@users.noreply.github.com>
2023-12-21 19:11:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function toCase(text) {
   // write your code here
+	let res = `${text.toLowerCase()} - ${text.toUpperCase()}`;
+	return(res);
 }
 
 // DO not change the code below
 
 const text = prompt("Enter text:");
-alert(toCase(text));
+alert(toCase(text)); 
